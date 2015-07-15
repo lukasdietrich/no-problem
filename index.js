36f550dfdbc31a3f64249f85036d291675fff279
Lukas Dietrich <lukas@lukasdietrich.com>
2015-07-15 18:57:07 +0200

    Running create script on startup

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+var fs       = require("fs");
+
 var octonode = require("octonode");
 var express  = require("express");
 var sqlite   = require("sqlite3");
+
+var db = new sqlite.Database("data.sqlite");
+
+fs.readFile("assets/sql/create.sql", { encoding: "ascii" }, function (err, data) {
+    if (err)
+        throw err;
+
+    db.exec(data);
+})
