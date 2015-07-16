84ef9407518b24f9ac60031148cb96fbeccc9d00
Lukas Dietrich <lukas@lukasdietrich.com>
2015-07-17 01:10:12 +0200

    Added users table to database scheme

--- assets/sql/create.sql
+++ assets/sql/create.sql
@@ -3,6 +3,14 @@ create table if not exists `settings` (
     `value` not null
 ) ;
 
+create table if not exists `users` (
+    `id` integer not null primary key ,
+    `role` integer not null ,
+    `token` varchar ( 96 ) not null ,
+    `username` varchar ( 64 ) not null ,
+    `displayName` varchar ( 96 ) not null
+) ;
+
 create table if not exists `mirrors` (
     `id` integer not null primary key autoincrement ,
     `repository` varchar ( 64 ) not null unique
